1e245f352d12f74d31de804c6cd4b0b0573eb1b2
Jethro Dean <jethroldean@gmail.com>
2022-03-29 16:16:07 +1100

    Add first prop component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from "react"
 import Nav from "./components/Nav"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import swimmer from "./images/swimmer.png"
 
 export default function App() {
     return (
         <div>
             <Nav />
             <Hero />
-            <Card />
+            <Card 
+                img={swimmer}
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life lessons with Katie Zaferes"
+                price={136}
+            />
         </div>
 
     )
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,47 +1,19 @@
 import React from "react"
-import swimmer from "../images/swimmer.png"
+
 import star from "../images/star.png"
 
 export default function Card(props) {
     return (
         <div className="card">
-            <img src={swimmer} alt="Swimmer photo" className="card--image" />
+            <img src={props.img} alt="Swimmer photo" className="card--image" />
             <div className="card--stats">
                 <img src={star} alt="Airbnb star" className="card--star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
-
-/* Prop Quiz:
-1. What do props help us accomplish? 
-A: Makes component reusable and dynamic.
-
-2. How do you pass a prop into a component? 
-A: <Component 
-        name = ...
-        DOB = ... 
-    />
-Variable names can be anything you'd like. 
-
-3. Can I pass a custom prop (e.g. 'blahblahblah={true}') to a native DOM element? (e.g. <div blahblahblah={true}>) Why or why not? 
-A: No, becuase the custom prop is not defined in the native language. 
-
-4. How do I recieve props in a component? 
-   function Navbar() {
-   return (
-       <header>
-           ...
-       </header>
-   )
-}
-A: Add props to paramater of function, then add {props.variablename} into the element. 
-
-5. What data tuype is 'props' when the compnent receives it? 
-A: Object.
-*/
